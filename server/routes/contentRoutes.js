3d92eef9f6ab448952511193192497cfc92d5182
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2019-07-29 20:45:01 +0530

    removed unnecesary files

--- server/routes/contentRoutes.js
+++
@@ -1,7 +0,0 @@
-var requireLogin = require('../middleware/requireLogin');
-var courseService = require('../services/content');
-
-module.exports = app => {
-    app.get("/courses/:courseName",requireLogin,courseService.renderCourses);
-    app.get("/courses/:courseName/:content",requireLogin,courseService.renderCourseContent);
-}
--- server/routes/emailVeriRoutes.js
+++
@@ -1,6 +0,0 @@
-const verficationServices = require("../services/emailVerification");
-
-module.exports = app => {
-    app.get("/confirmation",verficationServices.confirmEmail);
-    app.post("/resend",verficationServices.resendEmail);
-}
