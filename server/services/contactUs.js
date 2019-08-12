ff46f3d8ce5b5cb0a84d8da4668a3329c20245f5
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2019-08-12 16:35:50 +0530

    merge conflict : accept all incoming

--- server/services/contactUs.js
+++ server/services/contactUs.js
@@ -3,13 +3,8 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
 service: 'gmail',
   auth: {
-<<<<<<< HEAD
-    user: process.env.EMAILID,
-    pass: process.env.PASSWORD
-=======
     user: process.env.SUPP_EMAILER_ID,
     pass: process.env.SUPP_EMAILER_PSD
->>>>>>> local
   }
 });
 
@@ -18,17 +13,10 @@ exports.contactUs = async (req, res) => {
   console.log(req.body);
   res.redirect("/contact");
   let info = await transporter.sendMail({
-<<<<<<< HEAD
-    from: process.env.EMAILID, // sender address
-    to: "rudresh@xinfin.org", // list of receivers
-    subject: `Supp. Req. from ${req.body.email}`, // Subject line
-    html: `<div>Subject: ${req.body.subject} <br> Message: ${req.body.message}</div>` // html body
-=======
     from: process.env.SUPP_EMAILER_ID, 
     to: process.env.SUPP_EMAIL_ID, // support email id for blockdegree
     subject: `Supp. Req. from ${req.body.email}`, 
     html: `<div>Subject: ${req.body.subject} <br> Message: ${req.body.message}</div>` 
->>>>>>> local
   });
   console.log("Message sent: %s", info.messageId);
 };
