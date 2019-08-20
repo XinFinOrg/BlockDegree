16ee353d2710eaed408427f13e0d102d6243539f
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2019-08-20 16:08:24 +0530

    added args for retriveCertificate

--- server/services/retriveCertificate.js
+++ server/services/retriveCertificate.js
@@ -102,7 +102,7 @@ exports.downloadCertificate = async (req, res) => {
         files.forEach(async file => {
           var localPath = "tmp/" + file.path + ".png";
           imgHTML = file.content.toString("utf-8");
-          const browser = await puppeteer.launch();
+          const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
           const page = await browser.newPage();
           await page.setViewport({
             width: 960,
