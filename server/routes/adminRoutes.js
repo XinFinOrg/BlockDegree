6be56b8ab7607ca403a6a302cbdfa25ba35f1e8b
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2019-08-21 12:04:21 +0530

    made migrate admin-only

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,11 +1,19 @@
 const requireLogin = require("../middleware/requireLogin");
 const requrieAdmin = require("../middleware/requireAdmin");
-const path = require("path")
-const adminPath = path.join(__dirname,"../admin/");
+const path = require("path");
+const adminPath = path.join(__dirname, "../admin/");
+const migrationService = require("../services/migrate");
 
 module.exports = app => {
   app.get("/admin", requireLogin, requrieAdmin, (req, res) => {
     console.log("Called admin");
     res.sendFile("index.html", { root: adminPath });
   });
+
+  app.get(
+    "/api/migrate",
+    requireLogin,
+    requrieAdmin,
+    migrationService.migrateFrom
+  );
 };
--- server/routes/contentRoutes.js
+++ server/routes/contentRoutes.js
@@ -1,11 +1,7 @@
-const migrationService = require("../services/migrate");
 var requireLogin = require('../middleware/requireLogin');
 var courseService = require('../services/content');
 
 module.exports = app => {
     app.get("/courses/:courseName",requireLogin,courseService.renderCourses);
     app.get("/courses/:courseName/:content",requireLogin,courseService.renderCourseContent);
-
-    // temp
-    app.get("/api/migrate",migrationService.migrateFrom);
 }
