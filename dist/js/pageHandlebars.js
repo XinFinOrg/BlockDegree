0316744ff46e6ec6b19846d2c72b418c7f387d89
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2019-08-27 16:35:17 +0530

    revert last commit

--- dist/js/pageHandlebars.js
+++ dist/js/pageHandlebars.js
@@ -1,104 +1,84 @@
 function runHandlebars(id, dataSrc, src) {
-  if (document.getElementById(id) != null) {
+  if(document.getElementById(id) != null) {
     let content = document.getElementById(id);
-    ajax_get(dataSrc, function(data) {
+    ajax_get(dataSrc, function(data){
+
       let source = document.getElementById(src).innerHTML,
-        template = Handlebars.compile(source);
+          template = Handlebars.compile(source);
 
       content.innerHTML = template(data);
     });
   }
 }
 
-Handlebars.registerHelper("url", function(options) {
-  let removeSpecial = options.replace(/[^\w\s]/gi, "");
+Handlebars.registerHelper('url', function(options) {
+  let removeSpecial = options.replace(/[^\w\s]/gi, '');
 
-  return removeSpecial.replace(/ +/g, "-").toLowerCase();
+  return removeSpecial.replace(/ +/g, '-').toLowerCase();
 });
 
-Handlebars.registerHelper("trimmed", info => {
+Handlebars.registerHelper('trimmed', (info) => {
   let length = 15,
-    splitString = info.split(" ");
+      splitString = info.split(' ');
 
-  let trimmedString =
-    splitString.length > length
-      ? splitString.slice(0, length).join(" ") + " ..."
-      : info;
+  let trimmedString = splitString.length > length ?
+                      splitString.slice(0, length).join(' ') + ' ...':
+                      info;
 
   return trimmedString;
 });
 
-Handlebars.registerHelper("trimString", (info, title, idx) => {
-  let length = 10,
-    splitString = info.split(" ");
 
-  let trimmedString =
-    splitString.length > length
-      ? splitString.slice(0, length).join(" ") +
-        '<a href class="dialog-btn" data-btn-idx="' +
-        idx +
-        '"> ...Read more </a>' +
-        '<div class="dialog" data-dialog-idx="' +
-        idx +
-        '" title="' +
-        title +
-        '">' +
-        info +
-        "</div>"
-      : info;
+Handlebars.registerHelper('trimString', (info, title, idx) => {
+  let length = 10,
+      splitString = info.split(' ');
+
+  let trimmedString = splitString.length > length ?
+                      splitString.slice(0, length).join(' ') +
+                      '<a href class="dialog-btn" data-btn-idx="'+ idx +'"> ...Read more </a>' +
+                      '<div class="dialog" data-dialog-idx="'+ idx +'" title="'+ title +'">' + info + '</div>' :
+                      info;
 
   return trimmedString;
 });
 
 Handlebars.registerHelper("inc", function(value, options) {
-  return parseInt(value) + 1;
+    return parseInt(value) + 1;
 });
 
 // Special case for login button
-(function() {
-  if (document.getElementById("login-btn") != null) {
-    let content = document.getElementById("login-btn");
+(function (){
+  if(document.getElementById('login-btn') != null) {
+    let content = document.getElementById('login-btn')
 
-    $.ajax({
-      method: "post",
-      url: "/api/getAuthStatus",
-      data: {},
-      success: auths => {
-        if (
-          auths.localAuth ||
-          auths.twitterAuth ||
-          auths.facebookAuth ||
-          auths.googleAuth ||
-          auths.linkedinAuth
-        ) {
-          console.log(`result: ${result}`);
-          console.log(`status: ${result.status}`);
-          let source = document.getElementById("nav-login").innerHTML,
+        $.ajax({
+          type: "GET",
+          url: '/api/current_user',
+          dataType: 'json',
+          success : function(result) {
+            console.log(`result: ${result}`)
+            console.log(`status: ${result.status}`)
+            let source = document.getElementById('nav-login').innerHTML,
             template = Handlebars.compile(source);
-          content.innerHTML = template(result.status);
-        }
-      },
-      error: function(err) {
-        console.log(err);
-        // window.location = "/";
-      }
-    });
-  }
+            content.innerHTML = template( result.status);
+          },
+          error: function(err) {
+            console.log(err);
+            window.location = '/';
+          }
+        });
+    };
 })();
 
 // runHandlebars(id, dataSrc, src)
-runHandlebars("nav-sub-1", "/data/courses.json", "nav-submenu-template");
+runHandlebars('nav-sub-1', '/data/courses.json', 'nav-submenu-template');
 
-runHandlebars(
-  "contributors",
-  "/data/contributors.json",
-  "contributors-template"
-);
+runHandlebars('contributors', '/data/contributors.json', 'contributors-template');
 
-runHandlebars("events", "/data/events.json", "event-template");
+runHandlebars('events', '/data/events.json', 'event-template');
 
-runHandlebars("courses", "/data/courses.json", "course-template");
+runHandlebars('courses', '/data/courses.json', 'course-template');
 
-runHandlebars("mod__details", "/data/courses.json", "module-template");
+runHandlebars('mod__details', '/data/courses.json', 'module-template');
 
-runHandlebars("partners", "/data/partners.json", "partners-details-template");
+runHandlebars('partners', '/data/partners.json', 'partners-details-template');
