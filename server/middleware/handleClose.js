598c7989f124018f8dc6c1eafba3ae909d664b0a
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2019-09-03 12:40:29 +0530

    added popup login optional

--- server/middleware/handleClose.js
+++ server/middleware/handleClose.js
@@ -1,7 +1,8 @@
 module.exports = (req, res, next) => {
-  if (req.query.close=="true"){
-      // its a pop-up, close on completion.
-      req.session.closeOnCallback=true;
+  console.log(req.query);
+  if (req.query.close == "true") {
+    // its a pop-up, close on completion.
+    req.session.closeOnCallback = true;
   }
   next();
 };
