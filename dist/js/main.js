921d6e6892c0433c15a4bbb7f6e24c9e55de2a37
Dattatray <dattatrayh@gmail.com>
2019-09-13 19:04:50 +0530

    Navigation UI changes
    Navigation UI changes and Login Logout btn changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -30,6 +30,49 @@ $(document).ready(function() {
     },
     speed: 400
   });
+  
+  
+  //------- Mobile Nav js starts --------//
+  if ($('#nav-menu-container').length) {
+    var $mobile_nav = $('#nav-menu-container').clone().prop({
+      id: 'mobile-nav'
+    });
+    $mobile_nav.find('> ul').attr({
+      'class': '',
+      'id': ''
+    });
+    $('body').append($mobile_nav);
+    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="lnr lnr-menu"></i></button>');
+    $('body').append('<div id="mobile-body-overly"></div>');
+    $('#mobile-nav').find('.menu-has-children').prepend('<i class="lnr lnr-chevron-down"></i>');
+
+    $(document).on('click', '.menu-has-children i', function(e) {
+      $(this).next().toggleClass('menu-item-active');
+      $(this).nextAll('ul').eq(0).slideToggle();
+      $(this).toggleClass("lnr-chevron-up lnr-chevron-down");
+    });
+
+    $(document).on('click', '#mobile-nav-toggle', function(e) {
+      $('body').toggleClass('mobile-nav-active');
+      $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
+      $('#mobile-body-overly').toggle();
+    });
+
+    $(document).click(function(e) {
+      var container = $("#mobile-nav, #mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
+          $('#mobile-body-overly').fadeOut();
+        }
+      }
+    });
+  } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
+    $("#mobile-nav, #mobile-nav-toggle").hide();
+  }
+  //------- Mobile Nav js ends --------//
+  
 
   //------- Tabs Js --------//
   if (document.getElementById("horizontalTab")) {
@@ -38,58 +81,8 @@ $(document).ready(function() {
       duration: 200
     });
   }
-
-  //------- Mobile Nav  js --------//
-
-  if ($("#nav-menu-container").length) {
-    var $mobile_nav = $("#nav-menu-container")
-      .clone()
-      .prop({
-        id: "mobile-nav"
-      });
-    $mobile_nav.find("> ul").attr({
-      class: "",
-      id: ""
-    });
-    $("body").append($mobile_nav);
-    $("body").prepend(
-      '<button type="button" id="mobile-nav-toggle"><i class="lnr lnr-menu"></i></button>'
-    );
-    $("body").append('<div id="mobile-body-overly"></div>');
-    $("#mobile-nav")
-      .find(".menu-has-children")
-      .prepend('<i class="lnr lnr-chevron-down"></i>');
-
-    $(document).on("click", ".menu-has-children i", function(e) {
-      $(this)
-        .next()
-        .toggleClass("menu-item-active");
-      $(this)
-        .nextAll("ul")
-        .eq(0)
-        .slideToggle();
-      $(this).toggleClass("lnr-chevron-up lnr-chevron-down");
-    });
-
-    $(document).on("click", "#mobile-nav-toggle", function(e) {
-      $("body").toggleClass("mobile-nav-active");
-      $("#mobile-nav-toggle i").toggleClass("lnr-cross lnr-menu");
-      $("#mobile-body-overly").toggle();
-    });
-
-    $(document).on("click", function(e) {
-      var container = $("#mobile-nav, #mobile-nav-toggle");
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($("body").hasClass("mobile-nav-active")) {
-          $("body").removeClass("mobile-nav-active");
-          $("#mobile-nav-toggle i").toggleClass("lnr-cross lnr-menu");
-          $("#mobile-body-overly").fadeOut();
-        }
-      }
-    });
-  } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
-    $("#mobile-nav, #mobile-nav-toggle").hide();
-  }
+  
+  
 
   //------- Smooth Scroll  js --------//
 
