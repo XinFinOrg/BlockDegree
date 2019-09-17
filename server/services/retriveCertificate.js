d2f31d360afde5a9e6aaefc6e2d403f73c765780
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2019-09-17 17:54:37 +0530

    started optimizations for shareSocials, retriveCertificate

--- server/services/retriveCertificate.js
+++ server/services/retriveCertificate.js
@@ -48,18 +48,22 @@ exports.getAllCertificates = async (req, res) => {
 
 exports.getCertificatesFromCourse = async (req, res) => {
   if (req.body.course == "" || req.body.course == undefined) {
-    res.status(400).json({
+    return res.status(400).json({
       error: "bad request: req.body.course id empty / undefined",
       status: 400
     });
   }
   const course = req.body.course;
-  const user = await User.findOne({ email: req.user.email }).catch(err => {
-    res.status(500).json({
+  let user;
+  try {
+    user = await User.findOne({ email: req.user.email });
+  } catch (e) {
+    console.error("Exception while fetching user: ", e);
+    return res.status(500).json({
       error: "DB is under maintainence pls try again after sometime",
       status: 500
     });
-  });
+  }
   let certificateHash = [{}];
   for (obj of user.examData.certificateHash) {
     if (obj.examType == course) {
@@ -75,7 +79,7 @@ exports.getCertificatesFromCourse = async (req, res) => {
 // get_user -> validate_hash -> get_user -> fetch_hash_frpm_IPFS -> get_screenshot -> save_screenshot -> send_screenshot -> delete_screenshot
 exports.downloadCertificate = async (req, res) => {
   if (req.body.hash == "" || req.body.hash == undefined) {
-    res.status(400).json({
+    return res.status(400).json({
       error: "please provide certificate hash",
       status: 400,
       uploaded: false
@@ -83,8 +87,11 @@ exports.downloadCertificate = async (req, res) => {
   }
   const hash = req.body.hash;
   let imgHTML = "";
-  const user = await User.findOne({ email: req.user.email }).catch(err => {
-    console.log("Error while fetching the user from mongodb: ", err);
+  let user;
+  try {
+    user = await User.findOne({ email: req.user.email });
+  } catch (e) {
+    console.log("Error while fetching the user from mongodb: ", e);
     return res.status(500).json({
       certificateHash: null,
       status: 500,
@@ -92,9 +99,9 @@ exports.downloadCertificate = async (req, res) => {
       error:
         "looks like our database is under maintenance, please try again after some time"
     });
-  });
+  }
   if (!user) {
-    return res.redirect("/login");
+    return res.status(400).json({ error: "not able to fetch user" });
   }
   for (obj of user.examData.certificateHash) {
     if (obj.clientHash != undefined && obj.clientHash == hash) {
@@ -105,33 +112,54 @@ exports.downloadCertificate = async (req, res) => {
         files.forEach(async file => {
           let localPath = "tmp/" + file.path + ".png";
           imgHTML = file.content.toString("utf-8");
-          const browser = await puppeteer.launch({
-            args: ["--no-sandbox", "--disable-setuid-sandbox"]
-          });
-          const page = await browser.newPage();
-          await page.setViewport({
-            width: 800,
-            height: 600,
-            deviceScaleFactor: 1
-          });
-          await page.setContent(imgHTML);
-          await page.screenshot({ path: localPath });
-          browser.close().then(() => {
-            res.download(localPath, function(errDownload) {
-              if (errDownload != null || errDownload != undefined) {
-                console.log(
-                  `Error sending download : ${localPath} : ${errDownload}`
-                );
-              }
-              fs.unlink(localPath, errUnlink => {
-                if (errUnlink != null || errUnlink != undefined) {
+          let browser;
+          try {
+            browser = await puppeteer.launch({
+              args: ["--no-sandbox", "--disable-setuid-sandbox"]
+            });
+            const page = await browser.newPage();
+            await page.setViewport({
+              width: 800,
+              height: 600,
+              deviceScaleFactor: 1
+            });
+            await page.setContent(imgHTML);
+            await page.screenshot({ path: localPath });
+          } catch (e) {
+            console.log("Error while taking screenshot from browser: ", e);
+            return res.status(500).json({
+              certificateHash: null,
+              status: 500,
+              uploaded: false,
+              error:
+                "Looks like something went wrong, please try again after sometime or contact us via contact-us page"
+            });
+          }
+          browser
+            .close()
+            .then(() => {
+              return res.download(localPath, function(errDownload) {
+                if (errDownload != null || errDownload != undefined) {
                   console.log(
-                    `Error while deleting : ${localPath} : ${errUnlink}`
+                    `Error sending download : ${localPath} : ${errDownload}`
                   );
                 }
+                fs.unlink(localPath, errUnlink => {
+                  if (errUnlink != null || errUnlink != undefined) {
+                    console.log(
+                      `Error while deleting : ${localPath} : ${errUnlink}`
+                    );
+                  }
+                });
+              });
+            })
+            .catch(e => {
+              console.log(`exception at browser.close(): `, e);
+              return res.status(500).json({
+                error:
+                  "some error has occured please try again later or contact us via contact-us page"
               });
             });
-          });
         });
       });
     }
