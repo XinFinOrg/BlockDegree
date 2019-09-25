d9905d38a1b041c698a5c11899546fe941639e08
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2019-09-25 10:50:44 +0530

    exam: opposite conditions set for exams: testing

--- server/services/exam.js
+++ server/services/exam.js
@@ -65,9 +65,8 @@ exports.submitExam = async (req, res, next) => {
                 parseInt(request[index]) + 1 ==
                 result.questionsBasic[index].answer
               ) {
-                // marks++;
+                marks++;
               }
-              marks++;
             }
             attempts += 1;
             User.findOneAndUpdate(
@@ -129,9 +128,8 @@ exports.submitExam = async (req, res, next) => {
                 parseInt(req.body[index]) + 1 ==
                 result.questionsAdvanced[index].answer
               ) {
-                // marks++;
+                marks++;
               }
-              marks++;
             }
             attemptsAdvanced += 1;
             console.log("Marks", marks);
@@ -194,8 +192,9 @@ exports.submitExam = async (req, res, next) => {
                 parseInt(request[index]) + 1 ==
                 result.questionsProfessional[index].answer
               ) {
-                marks++;
+                // marks++;
               }
+              marks++;
             }
             attemptsProfessional += 1;
             User.findOneAndUpdate(
