ede83af5fb88d10bd4ed595e99ae73a82510b099
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2020-04-17 14:52:36 +0530

    donationListener: added logs

--- server/listeners/donationListener.js
+++ server/listeners/donationListener.js
@@ -203,7 +203,7 @@ xdc3.eth.subscribe("newBlockHeaders").on("data", async (result) => {
   try {    
     let retryCount = 0;
     let txCount = await xdc3.eth.getBlockTransactionCount(result.number);
-    // console.log(`[*] syncing block ${result.number} TX count: `, txCount);
+    console.log(`[*] syncing block ${result.number} TX count: `, txCount);
 
     while (txCount === null) {
       if (retryCount === 10) {
@@ -234,7 +234,7 @@ xdc3.eth.subscribe("newBlockHeaders").on("data", async (result) => {
           if (equateAddress(recipients[i].address, currBlockTx.to)) {
             currIndex = i;
           }
-        }
+        }        
 
         if (currIndex > -1) {
           console.log(`got anonymous deposit`);
