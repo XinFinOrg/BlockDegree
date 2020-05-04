eac36ca181ccfd9407ca8f54ca79a005d4b7bf88
rudreshsolank97 <rudreshsolanki101@gmail.com>
2020-05-05 01:01:01 +0530

    index: deferring all non-main images

--- dist/js/home.js
+++ dist/js/home.js
@@ -194,3 +194,13 @@ function animateNumberIncrease(currVal, desiredVal, elemId) {
     document.getElementById(elemId).innerHTML = exisVal + incr;
   }, 30);
 }
+
+function init() {  
+  let imgDefer = document.getElementsByTagName("img");
+  for (let i = 0; i < imgDefer.length; i++) {
+    if (imgDefer[i].getAttribute("data-src")) {
+      imgDefer[i].setAttribute("src", imgDefer[i].getAttribute("data-src"));
+    }
+  }
+}
+window.onload = init;
