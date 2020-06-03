48dbdd45b4f4444834a44a7732a1d327e0131af0
rudreshsolank97 <rudreshsolanki101@gmail.com>
2020-06-03 18:48:01 +0530

    updated gulp build bash file

--- gulpBuild.sh
+++ gulpBuild.sh
@@ -1,7 +1,7 @@
 gulp build
-if [[ -d /var/www/uat.blockdegree.org/ ]]; then
-  rm -r /var/www/uat.blockdegree.org/*
+if [[ -d /var/www/newuat.blockdegree.org/ ]]; then
+  rm -r /var/www/newuat.blockdegree.org/*
 else
-  mkdir /var/www/uat.blockdegree.org/
+  mkdir /var/www/newuat.blockdegree.org/
 fi
-cp -r ./dist/* /var/www/uat.blockdegree.org/
+cp -r ./dist/* /var/www/newuat.blockdegree.org/
